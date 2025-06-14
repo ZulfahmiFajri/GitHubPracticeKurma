063c5b2df892871c64b0246afe42fd6eccbdc7e1
ZulfahmiFjr <mccraft874@gmail.com>
2025-06-14 22:32:46 +0700

    edit main.c

--- main.c
+++ main.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 
-int add(int a, int b) { return a + b; }
-int sub(int a, int b) { return a - b; }
-int mul(int a, int b) { return a * b; }
-float div(int a, int b) {
-    if (b == 0) {
-        printf("Error: Pembagian dengan nol tidak diperbolehkan.\n");
-        return 0;
-    }
-    return (float)a / b;
-}
-
-int main(){
-    int a, b, pilihan;
+int main() {
+    int pilihan;
     
     while (1) {
         printf("\n==== Kalkulator Sederhana ====\n");
@@ -23,30 +12,9 @@ int main(){
         printf("5. Keluar\n");
         printf("Pilih operasi (1-5): ");
         scanf("%d", &pilihan);
-
-        if (pilihan == 5) {
+         if (pilihan == 5) {
             printf("Terima kasih telah menggunakan kalkulator.\n");
             break;
-        }
-
-        printf("Masukkan dua angka: ");
-        scanf("%d %d", &a, &b);
-
-        switch (pilihan) {
-            case 1:
-                printf("Hasil: %d\n", add(a, b));
-                break;
-            case 2:
-                printf("Hasil: %d\n", sub(a, b));
-                break;
-            case 3:
-                printf("Hasil: %d\n", mul(a, b));
-                break;
-            case 4:
-                printf("Hasil: %.2f\n", div(a, b));
-                break;
-            default:
-                printf("Pilihan tidak valid.\n");
         }
     }
     return 0;
